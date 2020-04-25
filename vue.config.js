d186789a26eafc77c7ace95221f91920b07f5b19
Gabriel Andrade Michalski <g.amichalski@gmail.com>
2020-04-25 13:03:45 +0000

    habilitado lint fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule("eslint")
+      .use("eslint-loader")
+      .options({ fix: true });
+  },
   devServer: {
     host: "0.0.0.0",
     hot: true,
